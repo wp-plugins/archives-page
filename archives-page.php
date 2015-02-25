0da0cf7762e23eaeb6b1f6c18363c181ad4adbec
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 22:58:00 +0000

    archives-page readme
    git-svn-id: https://plugins.svn.wordpress.org/archives-page/trunk@1099726 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ archives-page.php
@@ -0,0 +1,304 @@
+<?php
+/*
+Plugin Name: Archives Page
+Plugin URI: http://wp-time.com/archives/
+Description: Make archives page easily with full customize and in all languages of the world.
+Version: 1.0.0
+Author: Qassim Hassan
+Author URI: http://qass.im
+License: GPLv2 or later
+*/
+
+/*  Copyright 2015  Qassim Hassan  (email : qassim.pay@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+/* Some Functions */
+// check if blog posts equal 1 or more than 1
+function more__than__1__post(){
+	$count_posts = wp_count_posts();
+	$get_posts_count = $count_posts->publish;
+	return $get_posts_count >= 1;
+}
+
+// check if blog pages equal 1 or more than 1
+function more__than__1__page(){
+	$count_pages = wp_count_posts('page');
+	$get_pages_count = $count_pages->publish;
+	return $get_pages_count >= 1;
+}
+
+// get latest posts
+function get__latest__posts($posts_number = '10'){
+
+	$latest_post_args = array( 'numberposts' => $posts_number, 'post_type' => 'post', 'post_status' => 'publish' );
+	$recent_posts = wp_get_recent_posts( $latest_post_args );
+	$html = '';
+	
+	foreach( $recent_posts as $recent ){
+		$html .= '<li><a href="' . get_permalink($recent["ID"]) . '" title="'.esc_attr($recent["post_title"]).'">'.$recent["post_title"].'</a></li>';
+	}
+	
+	return $html;
+}
+
+// get tags list
+function get__tags__list($tags_number = '10'){
+	
+	$tags = get_tags("orderby=count&hide_empty=1&number=$tags_number");
+	$html = '';
+	
+	foreach ( $tags as $tag ) {
+		$tag_link = get_tag_link( $tag->term_id );	
+		$html .= "<li><a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug}'>{$tag->name}</a></li>";
+	}
+	
+	return $html;
+}
+
+
+/* Shortcodes */
+// daily archive shortcode
+function daily__archive( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Daily Archive', // default title is Daily Archive.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php if ( more__than__1__post() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= wp_get_archives( array( 'type' => 'daily', 'limit' => $number, 'echo' => 0 ) );
+				$result .= "</$list>";
+				return $result;
+            ?>
+		<?php endif; ?>
+    <?php
+
+}
+add_shortcode("daily__archive", "daily__archive");
+
+// monthly archive shortcode
+function monthly__archive( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Monthly Archive', // default title is Monthly Archive.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php if ( more__than__1__post() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= wp_get_archives( array( 'type' => 'monthly', 'limit' => $number, 'echo' => 0 ) );
+				$result .= "</$list>";
+				return $result;
+            ?>
+		<?php endif; ?>
+    <?php
+
+}
+add_shortcode("monthly__archive", "monthly__archive");
+
+// yearly archive shortcode
+function yearly__archive( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Yearly Archive', // default title is Yearly Archive.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php if ( more__than__1__post() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= wp_get_archives( array( 'type' => 'yearly', 'limit' => $number, 'echo' => 0 ) );
+				$result .= "</$list>";
+				return $result;
+            ?>
+		<?php endif; ?>
+    <?php
+
+}
+add_shortcode("yearly__archive", "yearly__archive");
+
+// latest posts shortcode
+function latest__posts( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'10', // default is 10 posts.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Latest Posts', // default title is Latest Posts.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php if ( more__than__1__post() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= get__latest__posts($number);
+				$result .= "</$list>";
+				return $result;
+            ?>
+		<?php endif; ?>
+    <?php
+
+}
+add_shortcode("latest__posts", "latest__posts");
+
+// get cats shortcode
+function get__cats( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Categories', // default title is Categories.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php 
+			$result  = "<$heading>$title</$heading>";
+			$result .= "<$list>";
+			$result .= wp_list_categories("title_li=0&number=$number&echo=0");
+			$result .= "</$list>";
+			return $result;
+		?>
+    <?php
+
+}
+add_shortcode("get__cats", "get__cats");
+
+// get tags shortcode
+function get__tags( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'10', // default is 10 tags.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Tags', // default title is Tags.
+			),$atts
+		)
+	);
+	
+	?>
+    	<?php if( wp_count_terms('post_tag', 'hide_empty=1') >= 1 ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= get__tags__list($number);
+				$result .= "</$list>";
+				return $result;
+            ?>
+        <?php endif; ?>
+    <?php
+
+}
+add_shortcode("get__tags", "get__tags");
+
+// get pages shortcode
+function get__pages( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'My Pages', // default title is My Pages.
+			),$atts
+		)
+	);
+	
+	?>
+    	<?php if( more__than__1__page() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= wp_list_pages("title_li=0&number=$number&echo=0");
+				$result .= "</$list>";
+				return $result;
+            ?>
+        <?php endif; ?>
+    <?php
+
+}
+add_shortcode("get__pages", "get__pages");
+
+// get authors shortocode
+function get__authors( $atts, $content = null ){
+
+	extract(
+		shortcode_atts(
+			array(
+				"number"		=>	'', // default is no limit.
+				"heading"		=>	'h3', // default is <h3></h3>.
+				"list"			=>	'ul', // default is unordered list <ul></ul>.
+				"title"			=>	'Authors', // default title is Authors.
+			),$atts
+		)
+	);
+	
+	?>
+		<?php if ( more__than__1__post() ) : ?>
+			<?php 
+				$result  = "<$heading>$title</$heading>";
+				$result .= "<$list>";
+				$result .= wp_list_authors("hide_empty=1&show_fullname=1&style=list&optioncount=1&orderby=id&html=1&number=$number&echo=0");
+				$result .= "</$list>";
+				return $result;
+            ?>
+		<?php endif; ?>
+    <?php
+
+}
+add_shortcode("get__authors", "get__authors");
+
+?>
---
+++ readme.txt
@@ -0,0 +1,117 @@
+﻿=== Archives Page ===
+Contributors: Qassim.Dev
+Donate link: http://j.mp/plugins_donation
+Tags: archive, archives, archives page, archive page, archive template, archives template, page template, image, images, shortcode, Post, plugin, posts, page, widget, admin, sidebar, google, twitter, comments, customize, customizer custom customizing, full
+Requires at least: 3.0
+Tested up to: 4.1.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Make archives page easily with full customize and in all languages of the world.
+
+== Description ==
+
+### Archives Page  
+
+Make archives page easily with full customize and in all languages of the world.
+
+### The Features
+
+* Make Archives Page easily in minutes.
+* 8 Shortcodes.
+* Full Customize.
+* All languages of the world.
+
+### Live Demo
+
+* [http://wp-time.com/archives/](http://wp-time.com/archives/)
+
+### Rate The Plugin
+
+* [Please rate Featured Image Widget plugin](https://wordpress.org/support/view/plugin-reviews/archives-page#postform)
+
+### Download Davinci WordPress Blog Theme
+
+* [Responsive and Retina WordPress blog Theme with a lot of features](http://qass.im/my-plugins/davinci-a-premium-wordpress-blog-theme/)
+
+### Beautiful WordPress Themes
+
+* [Collection of 87 themes for only $69](http://j.mp/beautiful_themes)
+
+### My Premium WordPress Themes And Plugins
+
+* [My premium themes and plugins on Creative Market.](http://j.mp/1AS73zL)
+* [My premium themes and plugins on Themeforest.](http://j.mp/Perso_themes)
+
+### About
+
+* [The plugin designed and developed by Qassim Hassan.](http://qass.im)
+* [Qassim Hassan on Twitter.](https://twitter.com/Qassim_Dev)
+
+### More Plugins
+
+* [My Plugins](https://profiles.wordpress.org/qassimdev/#content-plugins)
+
+== Installation ==
+
+### Installation
+
+1. Upload 'archives-page' folder to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+### Shortcodes
+
+1. [daily__archive] shortcode to display daily archive.
+2. [monthly__archive] shortcode to display monthly archive.
+3. [yearly__archive] shortcode to display yearly archive.
+4. [latest__posts] shortcode to display latest posts.
+5. [get__cats] shortcode to display categories.
+6. [get__tags] shortcode to display tags.
+7. [get__pages] shortcode to display pages list.
+8. [get__authors] shortcode to display authors list.
+
+### Shortcodes Attributes
+
+1. number="" (number of list, example: number="3" will be display 3 latest posts or 3 tags .. etc, see default number in default usage section).
+2. heading="" (heading of title, example: heading="h1" default is h3).
+3. list="" (list type, example: list="ol" default is ul).
+4. title="" (title of list, example: title="Recent Posts" enter title using your languge :) ).
+* See usage to understand me.
+
+### Default Usage
+
+	[daily__archive] // default number is no limited.
+	[monthly__archive] // default number is no limited.
+	[yearly__archive] // default number is no limited.
+	[latest__posts] // default number is 10 posts.
+	[get__cats] // default number is no limited.
+	[get__tags] // default number is 10 tags.
+	[get__pages] // default number is no limited.
+	[get__authors] // default number is no limited.
+	
+### Custom Usage
+
+	[daily__archive number="7" heading="h5" list="ol" title="Archive By Day"]
+	[monthly__archive number="12" heading="h5" list="ol" title="Archive By Month"]
+	[yearly__archive number="10" heading="h5" list="ol" title="Archive By Year"]
+	[latest__posts number="5" heading="h5" list="ol" title="Recent Posts"]
+	[get__cats number="6" heading="h5" list="ol" title="Our Categories"]
+	[get__tags number="8" heading="h5" list="ol" title="Tags List"]
+	[get__pages number="9" heading="h5" list="ol" title="Our Pages"]
+	[get__authors number="15" heading="h5" list="ol" title="Our Authors"]
+
+== Frequently Asked Questions ==
+
+### FAQ
+
+* [For more questions or help, contact me.](http://qass.im/contact)
+* [Or ask me on twitter.](https://twitter.com/Qassim_Dev)
+
+== Changelog ==
+
+= 1.0.0 =
+First version.
+
+== Upgrade Notice ==
+Currently not available.
